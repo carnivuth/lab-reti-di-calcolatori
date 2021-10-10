3f63d851b5ee1af4b5a4da51ec312d8a13dca7a3
Longhi Matteo <62444605+carnivuth@users.noreply.github.com>
2021-10-10 17:04:24 +0200

    correzioni finali
    eliminazione stampe di debug, aggiunta commenti e spazioatura codice, cambio dei cicli do while in cicli while

--- esercitazione_0/consumatore.c
+++ esercitazione_0/consumatore.c
@@ -6,50 +6,54 @@
 #include <unistd.h>
 #define MAX_STRING_LENGTH 256
 
-// consumatore.c e' un filtro
+
 int main(int argc, char* argv[]){
 
 	char *file_in ,*filter=argv[1];
 	int c;
 	
-	//controllo numero argomenti
-	if (argc == 3){
+        if (argc == 3){
         // redirezione input e apertura del file
-        close(0);
-        file_in = argv[2];
-	    
-	    if (open(file_in, O_RDONLY)<0){
-    		perror("P0: Impossibile aprire il file.");exit(2);
-	    }    
-	}else if (argc != 2 ){ 
-		perror(" numero di argomenti sbagliato"); exit(1);
+                close(0);
+                file_in = argv[2];    
+	
+                if (open(file_in, O_RDONLY)<0){
+    	                perror("Impossibile aprire il file.");exit(2);
+	        }    
+	
+        }else if (argc != 2 ){ 
+		perror("parametri sbagliati,\n sintassi chiamata: consumatore filter file"); exit(1);
 	}
-        float times= (float)time(NULL);
-	//scrematura caratteri stringa di filtraggio
-	 char output[MAX_STRING_LENGTH];
+	
+        //scrematura caratteri stringa di filtraggio
+	char TFilter[MAX_STRING_LENGTH];
         int i=1,j=1;
-       // if (strlen(filter)==0) return 0;
-        output[0]=filter[0];
+        TFilter[0]=filter[0];
+        
         if (strlen(filter)>1){
+        
                 while(filter[i]!='\0'){
-                        if (strchr(output, filter[i])==NULL){
-                                output[j]=filter[i];
+        
+                        if (strchr(TFilter, filter[i])==NULL){
+                                TFilter[j]=filter[i];
                                 j++;
                         }
                         i++;
                 }
-                output[i]='\0';
-        }
-
-	
-
-	do{
-        c=getc(stdin);
-        if(strchr(output,c)==NULL && c!=EOF){
-            putchar(c);
+                TFilter[i]='\0';
         }
         
-    }while(c!=EOF);
-    printf("%f",(float)time(NULL)-times);   
-	printf("%s",output);
+        //main loop
+        c=getc(stdin);
+	
+        while(c!=EOF){
+        
+                if(strchr(TFilter,c)==NULL){
+                        putchar(c);
+                }
+                c=getc(stdin);
+        
+        }
+     
+
 }
--- esercitazione_0/produttore.c
+++ esercitazione_0/produttore.c
@@ -8,32 +8,35 @@
 
 // produttore.c NON e' un filtro
 int main(int argc, char* argv[]){
-	int fd, written ;
+	int c, fd, written ;
 	char *file_out;
+	
 	//controllo numero argomenti
 	if (argc != 2){ 
 		perror(" numero di argomenti sbagliato"); exit(1);
 	} 
-    //controllo apertura file 
+    
+	//controllo apertura file 
 	file_out = argv[1];	
 	fd = open(file_out, O_WRONLY|O_CREAT|O_TRUNC, 00640);
+	
 	if (fd < 0){
-		perror("P0: Impossibile creare/aprire il file");
+		perror("Impossibile creare/aprire il file");
 		exit(2);
-	}int c;
-   //main loop
-	written=write(fd, &c,4)
-	while(c!=EOF){
-		written=write(fd, &c,4);
-		if(written!=4){
-		    	perror("P0: errore nella scrittura sul file");
-			exit(3);
-		}
-        c=getc(stdin);
-       
-        
-       
-        }
+	}
+    
+	//main loop
+    c=getc(stdin);
+	
+	while(c!=EOF){ 
+        written=write(fd, &c,4);
+    
+	        if(written!=4){
+		    	perror("errore nella scrittura sul file");
+				close(fd);
+			    exit(3);
+		    }
+       c=getc(stdin); 
     }
 	close(fd);
 }
