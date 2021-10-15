c2a062fed1752a662cecb68a70d93fb4356c104c
Luca3700 <luca.torzi37@gmail.com>
2021-10-15 19:51:26 +0200

    Update TestRandomAccessFile.java
    Sto cercando di fare la swap, non è completa

--- esercitazione_1/TestRandomAccessFile.java
+++ esercitazione_1/TestRandomAccessFile.java
@@ -4,14 +4,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public class TestRandomAccessFile {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String line;
-		long pos=0;
-			try {
-				RandomAccessFile x= new RandomAccessFile("INSERIRE IL PATH DEL FILE", "rw");
+		int pos=0;
+			/*try {
+				RandomAccessFile x= new RandomAccessFile("C:/Users/39320/eclipse-workspace/RetiEsercitazione1/src/Swap/ciao.txt", "rw");
 				line=x.readLine();
 				int i=1;
 				while(line!=null) {
@@ -47,8 +46,115 @@ public class TestRandomAccessFile {
 				// TODO Auto-generated catch block
 				System.out.println("\nFuck go back2");
 				e.printStackTrace();
+			}*/
+		
+		
+		int numLinee=0;
+		String stringa;
+		int[] offsets= new int[20];
+		offsets[0]=0;
+		byte[] array= new byte[2];
+		try {
+			pos=0;
+			RandomAccessFile x= new RandomAccessFile("C:/Users/39320/eclipse-workspace/RetiEsercitazione1/src/Swap/ciao.txt", "rw");
+			while (x.read(array, 0, 1)>0) {
+				stringa=new String(array, "UTF-8");
+				System.out.print(stringa.charAt(0)); //I print all the file char for char
+				pos++; //numero di caratteri per linea
+				if (stringa.charAt(0)=='\n') {
+					numLinee++;
+					offsets[numLinee]=pos; //salvo gli offset
+					//pos=0;
+				}
 			}
-			System.out.println("FINE");
+			offsets[numLinee+1]=pos;
+			
+			
+			int linea1=1;
+			int linea2=4;
+					
+			//in base alla riga che sposto uso gli offset
+			//sono uguali alla fine della loro riga,  l'inizio della successiva
+			int offset1, offset2;
+			offset1=offsets[linea1];//offsets[line1-1] per ottenere da dove iniziare a leggere
+			offset2=offsets[linea2]; 
+			
+			int length1=offsets[linea1]-offsets[linea1-1];//lungazza delle due linee
+			int length2=offsets[linea2]-offsets[linea2-1];
+						
+			x.seek((long)offsets[linea1-1]);
+			String stringa1=x.readLine();
+			x.seek(0);
+			
+			x.seek((long)offsets[linea2-1]);
+			String stringa2=x.readLine();
+			x.seek(0);
+			
+			int differenza=length1-length2;
+			int posizione=0; //nuovo offsets
+			int lineaAttuale=0;
+			
+			
+			if (length1>=length2) { //scrivo dalla prima linea
+				//x.seek((long)offset1);
+				//x.writeUTF(stringa2);
+				//x.write(x., off, len);
+				lineaAttuale=linea1;
+				//char[] buffer = stringa2.toCharArray();
+				byte[] b = new byte[256];
+				//for (int i = 0; i < buffer.length; i++) {
+				//	  b[i] = (byte) buffer[i];
+				//	 }
+				x.seek((long)offsets[linea1-1]);
+				x.write(stringa2.getBytes(), 0/*offsets[linea1-1]*/, stringa2.getBytes().length);
+				x.write(System.lineSeparator().getBytes(), 0, System.lineSeparator().getBytes().length);
+					//forse bisogna scrivere anche lo /n
+				
+				//char aCapo=System.lineSeparator().getBytes();
+				posizione=offsets[linea1]-differenza+1; //perchè ho swappato le linee e devo scrivere a partire dalla fine della seconda
+				lineaAttuale++;
+				while(lineaAttuale<linea2) {
+					x.seek((long)offsets[lineaAttuale-1]);
+					x.read(b, 0/*offsets[lineaAttuale-1]*/, offsets[lineaAttuale]-offsets[lineaAttuale-1]);
+					String tmp=new String(b, "UTF-8");
+					System.out.println("Ho letto: "+tmp);
+					x.seek(posizione);
+					x.write(b, 0/*posizione*/, offsets[lineaAttuale]-offsets[lineaAttuale-1]);
+					//idem forse va scritto anche /n
+					posizione=posizione+(offsets[lineaAttuale]-offsets[lineaAttuale-1]);
+					lineaAttuale++;
+				}
+				System.exit(1);
+				
+				x.write(stringa1.getBytes(), offsets[linea2]-differenza, stringa1.getBytes().length);
+					//forse bisogna scrivere anche lo /n
+				
+				
+				
+				
+				//lineaAttuale=linea1;
+				//posizione=offset2; //perchè li ho swappati
+				//while (lineaAttuale);
+			}
+			else { //scrivo dalla seconda linea
+				
+			}
+			
+			x.close();
+			
+			
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("\nFuck go back");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("\nFuck go back2");
+			e.printStackTrace();
+		}
+		
+		System.out.println("FINE");
 	}
 
 }
+
