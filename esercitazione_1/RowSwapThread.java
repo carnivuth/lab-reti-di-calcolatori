ae1a15fee8f0a1565509bcd4e53e75600ec15ad0
Longhi Matteo <62444605+carnivuth@users.noreply.github.com>
2021-10-16 18:23:42 +0200

    Update RowSwapThread.java

--- esercitazione_1/RowSwapThread.java
+++ esercitazione_1/RowSwapThread.java
@@ -27,6 +27,7 @@ public class RowSwapThread extends Thread{
 	public void run() {
 		
 		try {
+			int LSLength=System.lineSeparator().getBytes().length;
 			DatagramSocket sock = new DatagramSocket(port);
 			byte[] buff = new byte[256];
 			DatagramPacket packet = new DatagramPacket(buff, buff.length);
@@ -44,8 +45,6 @@ public class RowSwapThread extends Thread{
 				//conversione linee da scambiare
 				int Linea1=Integer.parseInt(lines[0]);
 				int Linea2=Integer.parseInt(lines[1]);
-				
-				int LSLength=System.lineSeparator().getBytes().length;
 				int numLinee=0;
 				long[] offsets= new long[2];
 				String stringa1;
@@ -82,49 +81,43 @@ public class RowSwapThread extends Thread{
 						numLinee++;
 					}
 					if(stringa1=="" || stringa2=="")System.exit(-1);
-						//calcolo differenza tra le stringhe
-						int differenza=(str1Length>str2Length)?str1Length-str2Length:str2Length-str1Length;
-						////////////hardcoding delle linee per debug
-						linea1=1;
-						linea2=3;
-						long posizione=0; //nuovo offsets
-						int lineaAttuale=0;
-						
-						
-						if (str1Length>=str2Length) { //scrivo dalla prima linea
-						
-							lineaAttuale=linea1;
-							//mi posiziono all'inizio della prima linea
-							FReaderWriter.seek((long)offsets[LINEA_1]);
-							//e ci scrivo la seconda (perchÃ¨ appunto vanno scambiate)
-							FReaderWriter.write(stringa2.getBytes(), 0, str2Length);
-							FReaderWriter.write(System.lineSeparator().getBytes(), 0, LSLength);
-							//avendo scritto la seconda linea sovrascrivendo la prima
-							//ed essendo la seconda piÃ¹ piccola, ho alcuni caratteri rimasti
-							//quindi mi posiziono all'inizio di questi caratteri
-							//tutto ciÃ² Ã¨ memorizzato in posizione, che di volta in volta verrÃ  incrementata
-							//cosÃ¬ sposto man mano indietro di alcuni caratteri (cioÃ¨ della differenza tra le due stringhe)
-							//tutte le linee
-							posizione=(long)(offsets[LINEA_1]+str1Length+LSLength-differenza);
-							lineaAttuale++;
-							while(FReaderWriter.getFilePointer()<(long)offsets[LINEA_2]) {
-								//mi sposto all'inizio della riga successiva
-								FReaderWriter.seek((long)offsets[lineaAttuale-1]);
-								//leggo la linea
-								strtmp=FReaderWriter.readLine();
-								System.out.println("Ho letto: "+strtmp);
-								//mi sposto dove devo iniziare a scriverla e la scrivo
-								FReaderWriter.seek(posizione);
-								FReaderWriter.writeBytes(strtmp);
-								FReaderWriter.write(System.lineSeparator().getBytes(), 0, System.lineSeparator().getBytes().length);
-								//aggiorno le variabili
-								posizione=posizione+(offsets[lineaAttuale]-offsets[lineaAttuale-1]);
-								lineaAttuale++;
-							}
-							
-							
-							FReaderWriter.write(stringa1.getBytes(), 0, stringa1.getBytes().length);
-						}
+			        //calcolo differenza tra le stringhe
+			        int differenza=(str1Length>str2Length)?str1Length-str2Length:str2Length-str1Length;
+			        long posizione=0; //nuovo offsets
+			        
+			        
+			        if (str1Length>=str2Length) { //scrivo dalla prima linea
+			        
+			          //lineaAttuale=linea1;
+			          //mi posiziono all'inizio della prima linea
+			          FReaderWriter.seek((long)offsets[LINEA_1]);
+			          //e ci scrivo la seconda (perchè appunto vanno scambiate)
+			          FReaderWriter.write(stringa2.getBytes(), 0, str2Length);
+			          FReaderWriter.write(System.lineSeparator().getBytes(), 0, LSLength);
+			          //avendo scritto la seconda linea sovrascrivendo la prima
+			          //ed essendo la seconda più piccola, ho alcuni caratteri rimasti
+			          //quindi mi posiziono all'inizio di questi caratteri
+			          //tutto ciò è memorizzato in posizione, che di volta in volta verrà incrementata
+			          //così sposto man mano indietro di alcuni caratteri (cioè della differenza tra le due stringhe)
+			          //tutte le linee
+			          posizione=(long)(offsets[LINEA_1]+str1Length+LSLength-differenza);
+			          //lineaAttuale++;
+			          while(FReaderWriter.getFilePointer()<((long)offsets[LINEA_2]-differenza)) {
+			            //mi sposto all'inizio della riga successiva
+			            FReaderWriter.seek(FReaderWriter.getFilePointer()+differenza);
+			            //leggo la linea
+			            strtmp=FReaderWriter.readLine();
+			            //mi sposto dove devo iniziare a scriverla e la scrivo
+			            FReaderWriter.seek(posizione);
+			            FReaderWriter.writeBytes(strtmp);
+			            FReaderWriter.write(System.lineSeparator().getBytes(), 0, LSLength);
+			            //aggiorno le variabili
+			            posizione=posizione+strtmp.length()+LSLength;
+			          }
+			          
+			          
+			          FReaderWriter.write(stringa1.getBytes(), 0, stringa1.getBytes().length);
+			        }
 						else { 
 							//inizializzo buffer di appoggio per la lettura
 							byte buffer1[]=new byte[differenza];
@@ -141,14 +134,14 @@ public class RowSwapThread extends Thread{
 							FReaderWriter.write(stringa2.getBytes(), 0,str2Length);
 							
 							//ciclo di riscrittura del file (sezione linea1-linea2)
-							while(FReaderWriter.getFilePointer()<(long)offsets[LINEA_2]+differenza) {
+							while(FReaderWriter.getFilePointer()<(long)offsets[LINEA_2]) {
 								FReaderWriter.read(buffer2, 0, differenza);
 								FReaderWriter.seek(FReaderWriter.getFilePointer()-differenza);
 								FReaderWriter.write(buffer1, 0,differenza);
 								buffer1=Arrays.copyOf(buffer2, buffer2.length);
 								
 							}
-							
+							FReaderWriter.write(buffer1, 0,(int)(offsets[LINEA_2]+differenza)-FReaderWriter.getFilePointer());
 						
 						
 						}
